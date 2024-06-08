1e55b8038cef5d41d320fdf1f86000c9137bf57e
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-08 17:44:42 +0300

    ex. 5 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
-//
-int main()
-{
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+
+template <typename T>
+T* createArray(int size) {
+    T* arr = new T[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 255;
+    }
+    return arr;
+}
+
+template <typename T>
+void printArray(T* arr, int size) {
+    if (arr != nullptr) {
+        for (int* i = new int{ 0 }; *i < size; *i += 1) {
+            cout << *(arr + *i) << " ";
+        }
+        cout << endl;
+    }
+}
+
+template <typename T>
+T* sum_Array(T* arr, int size, T* sum = new T{0}) {
+    if (arr != nullptr) {
+        for (int* i = new int{ 0 }; *i < size; *i += 1) {
+            *sum += *(arr + *i);
+        }
+        return sum;
+        delete sum;
+    }
+}
+
+
+int main() {
+    srand(time(0));
+    int* size = new int;
+    cout << "Enter the size of the array: ";
+    cin >> *size;
+    int* arr = createArray<int>(*size);
+    printArray(arr, *size);
+    int* sum = sum_Array<int>(arr, *size);;
+    cout << "Sum of array : " << *sum << endl;
+    delete[] arr;
+    delete size,sum;
+
+    return 0;
 }
