ff6a751f159f4ddea7ce951dc19c06eea86da2a9
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-08 17:48:08 +0300

    add nullptr check

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
-//
+
 int max_num(int* number_1, int* number_2) {
-	return (*number_1 > *number_2) ?  *number_1 : *number_2;
+	if (number_1 != nullptr && number_1 != nullptr) {
+		return (*number_1 > *number_2) ? *number_1 : *number_2;
+	}
 }
 
 
@@ -23,34 +25,3 @@ int main()
 	system("pause");
 	return 0;
 }
-
-
-//#include <iostream>
-//#include <ctime>
-//using namespace std;
-//
-//double* div(double* number_1, double* number_2) {
-//    return (*number_1 != 0 && *number_2 != 0) ? new double{ *number_1 / *number_2 } : nullptr;
-//}
-//
-//int main() {
-//
-//    double* number_1 = new double;
-//    double* number_2 = new double;
-//    double* result = nullptr;
-//
-//    cout << "Enter the first number: ";
-//    cin >> *number_1;
-//    cout << "Enter the second number: ";
-//    cin >> *number_2;
-//    result = div(number_1, number_2);
-//    (result != nullptr) ? cout << "Result: " << *result << endl : cout << "The result value is null!" << endl;
-//
-//    delete number_1;
-//    delete number_2;
-//    if (result != nullptr)
-//    {
-//        delete result;
-//    }
-//    return 0;
-//}
