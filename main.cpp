c63fb8fca97f8c5c433f0b5d392fcc4e0ea10985
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-08 17:24:15 +0300

    ex. 4 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
-//
-int main()
-{
-	cout << "Hello world" << endl;
-	system("pause");
-	return 0;
+
+double* calc(double* number_1, double* number_2, char* act) {
+    if (*act == '/') {
+        return (*number_1 != 0 && *number_2 != 0) ? new double{ *number_1 / *number_2 } : nullptr;
+    }
+    else if (*act == '*') {
+        return new double{ *number_1 * *number_2 };
+    }
+    else if (*act == '+') {
+        return new double{ *number_1 + *number_2 };
+    }
+    else if (*act == '-') {
+        return new double{ *number_1 - *number_2 };
+    }
+    /*else if (*act == '^') {
+     
+        double* temp = new double{ 1 };
+        int* i = new int{ 0 };
+        for (; *i < *number_2;)
+        {
+            *temp *= *number_1;
+            *i += 1;
+        }
+    }*/
+    else {
+        return nullptr;
+    } 
+}
+
+int main() {
+
+    double* number_1 = new double;
+    double* number_2 = new double;
+    char* action = new char;
+    double* result = nullptr;
+
+    cout << "Enter the first number: ";
+    cin >> *number_1;
+
+    cout << "Enter the action: ";
+    cin >> *action;
+
+    cout << "Enter the second number: ";
+    cin >> *number_2;
+    
+
+    result = calc(number_1, number_2, action);
+
+    (result != nullptr) ? cout << "Result: " << *result << endl : cout << "The result value is null!" << endl;
+
+    delete number_1;
+    delete number_2;
+    delete action;
+    if (result != nullptr)
+    {
+        delete result;
+    }
+    return 0;
 }
