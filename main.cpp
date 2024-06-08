84454058006d036e68bfe530388ab7c0338dfa48
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-08 16:48:30 +0300

    ex. 2 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 using namespace std;
 //
+
+void num_sign(int* number_1) {
+	int sign = (*number_1 != 0) ? 1 : 0;
+	if (*number_1 == 0) {
+		cout << "Your number is 0" << endl;
+	}
+	else {
+		(*number_1 > 0) ? cout << "Your number is positive" << endl : cout << "Your number is negative" << endl;
+	}
+}
+
+
+
 int main()
 {
-	cout << "Hello world" << endl;
+	int* num_1 = new int;
+	cout << "Enter the first number: ";
+	cin >> *num_1;
+
+	num_sign(num_1);
+	cout << "The end" << endl;
+	delete num_1;
 	system("pause");
 	return 0;
 }
+
