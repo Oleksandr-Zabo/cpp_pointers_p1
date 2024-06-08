5cdfd6c04cb3fee5990006eac9fb2116972128e9
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-06-08 16:57:05 +0300

    ex 3 lab had done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
-//
+
 int main()
 {
-	cout << "Hello world" << endl;
+	int* number_1 = new int;
+    int* number_2 = new int;
+    int* temp = new int;
+
+    cout << "Enter the first number: ";
+    cin >> *number_1;
+    cout << "Enter the second number: ";
+    cin >> *number_2;
+    *temp = *number_1;
+    *number_1 = *number_2;
+    *number_2 = *temp;
+    cout << "First number: " << *number_1 << endl
+        << "Second number: " << *number_2 << endl;
+
+	cout << "The end" << endl;
+    delete number_1;
+    delete number_2;
+    delete temp;
 	system("pause");
 	return 0;
 }
